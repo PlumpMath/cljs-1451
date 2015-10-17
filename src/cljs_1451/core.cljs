50e04d1565a157e997e4d99579d1293afc2d525e
Sebastian Bensusan <sbensu@gmail.com>
2015-10-17 17:30:02 +0200

    Reproducing issue

--- src/cljs_1451/core.cljs
+++ src/cljs_1451/core.cljs
@@ -1,9 +1,18 @@
 (ns cljs-1451.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [cljs-1451.foo :as foo]
+            [clojure.browser.repl :as repl]))
 
 ;; (defonce conn
 ;;   (repl/connect "http://localhost:9000/repl"))
 
 (enable-console-print!)
 
-(println "Hello world!")
+(println "Do I have a bug?")
+
+(defn main []
+  (reify
+    foo/SomeProtocol
+    (foo/method-one [_] "No you don't")
+    (foo/method-two [_] nil)))
+
+(println (foo/method-one (main)))
---
+++ src/cljs_1451/foo.cljs
@@ -0,0 +1,5 @@
+(ns cljs-1451.foo)
+
+(defprotocol SomeProtocol
+  (method-one [t] nil)
+  (method-two [t] nil))
